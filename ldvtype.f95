caf4189cde05dbeb3d578d51c310111fec9e419a
Ethan Mines <elmines@linux-02.ad.cbhp.ua.edu>
2016-11-16 17:18:49 -0600

    Correction: did not have ldvtype.f95 added before but do now

---
+++ ldvtype.f95
@@ -0,0 +1,39 @@
+SUBROUTINE LDVTYPE
+  IMPLICIT NONE
+
+  INTEGER :: Code, Records, BadRecords, rc
+  CHARACTER :: Data*15
+
+  OPEN(7, FILE = "vtype.data")
+  OPEN(8, FILE = "vtype.db", FORM = "FORMATTED", ACCESS = "DIRECT", RECL = 15)
+
+  Records = 0
+  BadRecords = 0
+
+  DO
+    READ (7, 100, IOSTAT = rc) Code, Data
+100   Format(I2, A15)
+    
+    IF (rc /= 0) EXIT
+
+    IF (Code < 1 .OR. 15 < Code) THEN
+      BadRecords = BadRecords + 1
+      CYCLE
+    END IF
+
+    WRITE(8, 200, REC = code + 1) Data
+200   Format(A15)
+    
+    Records = Records + 1
+  END DO
+
+  WRITE(8, 300, REC = 1) Records
+300  Format(I2.2)
+
+  WRITE(*, 400) Records, BadRecords
+400 Format("Type Records Written: ", I2.2, T30, "Bad Records: ", I2.2)
+
+  CLOSE(7)
+  CLOSE(8)
+
+END SUBROUTINE LDVTYPE
