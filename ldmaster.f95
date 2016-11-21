aac220b5346c8ccc08b6ecacb01f1e0c52046b03
Ethan Mines <elmines@linux-02.ad.cbhp.ua.edu>
2016-11-21 17:28:29 -0600

    Implement working ldmaster.f95 stand-alone program

--- ldmaster.f95
+++ ldmaster.f95
@@ -10,7 +10,7 @@ PROGRAM LDMASTER
 
   OPEN(7, FILE = "police1.data")
   OPEN(8, FILE = "police2.data")
-  OPEN(9, FILE = "master.db", FORM = "FORMATTED", ACCESS = "DIRECT", RECL = 110)
+  OPEN(9, FILE = "master.db", FORM = "FORMATTED", ACCESS = "DIRECT", RECL = 106)
 
   Counter = 0
 
@@ -23,8 +23,13 @@ PROGRAM LDMASTER
 
     Counter = Counter + 1
     WRITE(9, 200, REC = Counter + 1) Part1, Part2, Part3, Part4
+
+    !PRINT *, "Counter = ", Counter
+    !WRITE(*, 200) Part1, Part2, Part3, Part4
 200   FORMAT(A3, A2, A78, A23)             !Write the data extracted from the data file to the database, but without hyphens
   END DO
+
+PRINT *, "Records written after police1 loop: ", Counter
 
   !Repeat the process for police2.data
   DO 
@@ -34,7 +39,14 @@ PROGRAM LDMASTER
 
     Counter = Counter + 1
     WRITE(9, 200, REC = Counter + 1) Part1, Part2, Part3, Part4
+
+    !PRINT *, "Counter = ", Counter
+    !WRITE(*, 200) Part1, Part2, Part3, Part4
+
   END DO 
+
+
+PRINT *, "Records written after police2 loop: ", Counter
 
   WRITE(9, "(I4.4)", REC = 1) Counter
 
