fd5dc37c4d88f20e5aa98dd1e130b69c19c2417f
Ethan Mines <elmines@linux-02.ad.cbhp.ua.edu>
2016-11-28 11:55:04 -0600

    Get correct headings for dspmenu

--- dspmenu.f95
+++ dspmenu.f95
@@ -4,12 +4,12 @@ SUBROUTINE DSPMENU
   WRITE(*, "(/T15, A/)") "* * * Police Information System Main Menu * * *"
 
   WRITE(*, "(/T20, A)") "1 - Initial Data Load"
-  WRITE(*, "(/T20, A)") "2 - Display Records" 
-  WRITE(*, "(/T20, A)") "3 - Option 3"
-  WRITE(*, "(/T20, A)") "4 - Option 4"
-  WRITE(*, "(/T20, A)") "5 - Option 5"
-  WRITE(*, "(/T20, A)") "6 - Option 6"
-  WRITE(*, "(/T20, A)") "7 - Option 7"
+  WRITE(*, "(/T20, A)") "2 - Display Auxiliary Files" 
+  WRITE(*, "(/T20, A)") "3 - Display a Record (by SSN)" 
+  WRITE(*, "(/T20, A)") "4 - Add a Record"
+  WRITE(*, "(/T20, A)") "5 - Delete a Record"
+  WRITE(*, "(/T20, A)") "6 - Modify a Record"
+  WRITE(*, "(/T20, A)") "7 - List Master File"
   WRITE(*, "(/T20, A)") "8 - Quit"
 
 END SUBROUTINE DSPMENU
